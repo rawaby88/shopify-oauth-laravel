20743b993a5dc9abf1d4fb4f91e98fdd2a0ba95a
joymendonca <joymendonca1999@gmail.com>
2024-03-16 19:30:06 +0530

    feat: added hmac validation and registering uninstall webhooks

--- src/Http/Controllers/ShopifyLoadController.php
+++ src/Http/Controllers/ShopifyLoadController.php
@@ -14,14 +14,20 @@ class ShopifyLoadController extends Controller
 {
     public function load(): RedirectResponse
     {
-        if(request('code') && request('shop'))
+        if(request('code') && request('shop') && request('hmac'))
         {
+            if(!$this->validateResponse(request('hmac'), request()->getQueryString())){
+                return redirect()->route('error')->with([
+                    'message' => 'Invalid Request'
+                ]);
+            }
+
             $client_id = Config::get('shopify-oauth-laravel.client_id');
             $client_secret = Config::get('shopify-oauth-laravel.client_secret');
             $app_home_url = Config::get('shopify-oauth-laravel.app_home_url');
             $response = Http::post('https://' . request('shop') . '/admin/oauth/access_token?client_id=' . $client_id . '&client_secret=' . $client_secret . '&code=' . request('code'));
             if(isset($response['access_token'])){
-                $store = $this->getUserModelClass()::query()->updateOrCreate([
+                $store = $this->getStoreModelClass()::query()->updateOrCreate([
                     'store_url' => request('shop')
                 ],[
                     'store_url' => request('shop'),
@@ -34,6 +40,8 @@ class ShopifyLoadController extends Controller
                 if (isset($user->id) && isset($store->id)) {
                     if ($this->assignUserToStore($user->id, $store->id)) {
                         Auth::login($user);
+                        if(!$store->uninstall_webhook)
+                            $this->createUninstallWebhook(request('shop'), $response['access_token']);
                         ShopifyOauthLaravel::setStoreUrl($store->store_url);
                         ShopifyOauthLaravel::callInstallCallback($user, $store);
                         ShopifyOauthLaravel::callLoadCallback($user, $store);
@@ -52,12 +60,20 @@ class ShopifyLoadController extends Controller
         }
     }
 
+    protected function validateResponse($hmac, $query_string)
+    {
+        $client_secret = Config::get('shopify-oauth-laravel.client_secret');
+        $query_without_hmac = str_replace('hmac='. $hmac . '&', '', $query_string);
+        $calculated_hmac = hash_hmac('sha256', $query_without_hmac, $client_secret);
+        return $calculated_hmac == $hmac;
+    }
+
     protected function saveUserIfNotExist($store_url)
     {
         return $this->getUserModelClass()::query()->firstOrCreate([
-            'email' => 'store@'.$store_url
-        ],
-        [
+            'email' => 'store@'.$store_url,
+            'store_url' => $store_url
+        ], [
             'email' => 'store@'.$store_url,
             'store_url' => $store_url
         ]);
@@ -78,6 +94,23 @@ class ShopifyLoadController extends Controller
             'created_at' => now(),
             'updated_at' => now(),
         ]);
+    }
+
+    protected function createUninstallWebhook($store_url, $access_token)
+    {
+        $data['webhook'] = [
+            'address' => url('/shopify-app-auth/uninstall'),
+            'topic' => 'app/uninstalled',
+            'format' => 'json',
+        ];
+        $api_version = Config::get('shopify-oauth-laravel.api_version');
+        $response = Http::withHeader('X-Shopify-Access-Token', $access_token)->post('https://' . $store_url . '/admin/api/' . $api_version . '/webhooks.json', $data);
+        
+        if(isset($response['webhook']['id'])){
+            $this->getStoreModelClass()::query()->update([
+                'uninstall_webhook' => $response['webhook']['id']
+            ]);
+        }
     }
 
     protected function getUserModelClass(): string

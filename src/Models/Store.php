b9919557bbbff4cad320b5935d25363aab1894f0
joymendonca <joymendonca1999@gmail.com>
2024-03-16 16:06:01 +0530

    feat: added store model

---
+++ src/Models/Store.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ShopifyOauthLaravel\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Config;
+
+class Store extends Model
+{
+    protected $fillable = [
+        'store_url',
+        'access_token',
+        'scope',
+    ];
+
+    public function getTable()
+    {
+        return Config::get('shopify-oauth-laravel.tables.stores', parent::getTable());
+    }
+}

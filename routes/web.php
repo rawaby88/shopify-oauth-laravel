b72099c1afebc60948fd9b58d2d3c97c21c4ec4f
joymendonca <joymendonca1999@gmail.com>
2024-03-16 19:31:42 +0530

    feat: added uninstall route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
 
 Route::group([
-    'prefix' => 'auth'
+    'prefix' => 'shopify-app-auth'
 ], function () {
 
     Route::get('install', [Config::get('shopify-oauth-laravel.controllers.install'), 'install'])
@@ -12,5 +12,8 @@ Route::group([
 
     Route::get('load', [Config::get('shopify-oauth-laravel.controllers.load'), 'load'])
         ->name('shopify-load');
-        
+
+    Route::get('uninstall', [Config::get('shopify-oauth-laravel.controllers.uninstall'), 'uninstall'])
+        ->name('shopify-uninstall');
+
 });
